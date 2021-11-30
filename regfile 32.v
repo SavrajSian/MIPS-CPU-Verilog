89e2c53e630441de3ea7792efc6b119e6d237d23
Dinho-23 <94057520+Dinho-23@users.noreply.github.com>
2021-11-30 19:15:39 +0000

    Update regfile 32.v

--- regfile 32.v
+++ regfile 32.v
@@ -2,6 +2,7 @@ module regfile(
     input logic clk,
     input logic reset,
     input logic reg_dst,
+    input logic valid,
     input logic[31:0] Instruction,
     input logic[31:0] WriteData,
     input logic W_en,
@@ -19,15 +20,17 @@ assign read_addr_2 = Instruction[20:16];
 assign write_addr = reg_dst == 1 ? Instruction[15:11]:Instruction[20:16];
 
 always_ff @(posedge clk) begin
-    ReadData1 <= regs[read_addr_1];
-    ReadData2 <= regs[read_addr_2];
-    if (reset == 1) begin
-        for (integer idx = 0; idx<32; idx=idx+1) begin
-            regs[idx]<=0;
+    if valid==1:
+        ReadData1 <= regs[read_addr_1];
+        ReadData2 <= regs[read_addr_2];
+        if (reset == 1) begin
+            for (integer idx = 0; idx<32; idx=idx+1) begin
+                regs[idx]<=0;
+            end
         end
-    end
-    else if (W_en == 1) begin
-        regs[write_addr]<= write_addr;
+        else if (W_en == 1) begin
+            regs[write_addr]<= writeData;
+        end
     end
 end
 endmodule
