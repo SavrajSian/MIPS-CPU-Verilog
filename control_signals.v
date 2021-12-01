97ab1b3640373178c1faead90e332f5f3fb33a57
Dinho-23 <94057520+Dinho-23@users.noreply.github.com>
2021-12-01 09:50:58 +0000

    Add files via upload

---
+++ control_signals.v
@@ -0,0 +1,34 @@
+module control_signals(
+    input logic [31:0] instruction,
+    output logic MemRead,
+    output logic MemWrite,
+    output logic ALUSrc,
+    output logic RegDest,
+    output logic RegWrite,
+    output logic PCSrc,
+    output logic MemtoReg
+);
+
+always_comb begin
+    if //load instruction begin:
+        MemRead = 1;
+        ALUSrc = 1;
+        RegWrite = 1;
+        MemtoReg = 1;
+        MemWrite = 0;
+        RegDest = 0;
+    end
+    else if //store instruction begin:
+        MemRead = 0;
+        ALUSrc = 1;
+        RegWrite = 0;
+        MemWrite = 1;
+    end
+    else if //R instruction begin:
+        MemRead = 0;
+        ALUSrc = 0;
+        RegWrite = 1;
+        MemtoReg = 0;
+        RegDest = 1;
+        MemWrite = 0;
+end
