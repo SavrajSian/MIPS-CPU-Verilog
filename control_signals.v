810a10695c2f8269438558edb8cba9287bc790e5
Dinho-23 <94057520+Dinho-23@users.noreply.github.com>
2021-12-02 14:44:31 +0000

    Update control_signals.v

--- control_signals.v
+++ control_signals.v
@@ -1,12 +1,15 @@
 module control_signals(
     input logic [31:0] instruction,
+    input logic end_of_instruction_reg
+    input logic end_of_instruction_store
     output logic MemRead,
     output logic MemWrite,
     output logic ALUSrc,
     output logic RegDest,
     output logic RegWrite,
     output logic PCSrc,
-    output logic MemtoReg
+    output logic MemtoReg,
+    output logic fetch,
 );
 
 always_comb begin
@@ -17,12 +20,16 @@ always_comb begin
         MemtoReg = 1;
         MemWrite = 0;
         RegDest = 0;
+        fetch = (end_of_instruction_reg==1)?1,0;
     end
     else if //store instruction begin
         MemRead = 0;
         ALUSrc = 1;
         RegWrite = 0;
         MemWrite = 1;
+        always_ff@(negedge end_of_instruction_store) begin
+            fetch = 1;
+        end
     end
     else if //R instruction begin
         MemRead = 0;
@@ -31,6 +38,8 @@ always_comb begin
         MemtoReg = 0;
         RegDest = 1;
         MemWrite = 0;
+        fetch = (end_of_instruction_reg==1)?1,0;
     end
+    
 end
 endmodule
