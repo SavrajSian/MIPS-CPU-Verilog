edd128bf1427e594f2d87e27458ff2675a3181a7
si220 <94972352+si220@users.noreply.github.com>
2021-12-10 17:18:24 +0000

    Update ALU_WITH_MORE_INSTRUCTIONS.v

--- ALU/ALU_WITH_MORE_INSTRUCTIONS.v
+++ ALU/ALU_WITH_MORE_INSTRUCTIONS.v
@@ -14,6 +14,8 @@ module ALU(
 	assign immediateSE = {{16{instruction[15]}}, instruction[15:0]};
 	logic[31:0] immediateZE;
 	assign immediateZE = {16'b0, instruction[15:0]};
+	logic[4:0] shamt;
+	assign shamt = instruction[10:6];
 
 always_comb begin
 
@@ -28,7 +30,14 @@ always_comb begin
 			6'b100100: ALUResult = ReadData1 & ReadData2; //AND
 			6'b100101: ALUResult = ReadData1 | ReadData2; //OR
 			6'b101000: ALUResult = ReadData1 ^ ReadData2; //XOR
+			6'b000000: ALUResult = ReadData2 << shamt; //SLL
+			6'b000100: ALUResult = ReadData2 << ReadData1; //SLLV
+			6'b000011: ALUResult = ReadData2 >>> shamt; //SRA
+			6'b000111: ALUResult = ReadData2 >>> ReadData1; //SRAV
+			6'b000010: ALUResult = ReadData2 >> shamt; //SRL
+			6'b000110: ALUResult = ReadData2 >> ReadData1; //SRLV
 		endcase
+			
 	end
 
 	else if(opcode == 6'b001000)begin
@@ -36,8 +45,7 @@ always_comb begin
 	end	
 	
 	else if(opcode == 6'b100011 || opcode == 6'b101011)begin
-		ALUResult = ReadData1 + immediateSE; //LW and SW
-		//ALUResult = {{ALUResult[31:2]}, 2'b0}; //NOT CURRENTLY WORKING	
+		ALUResult = $floor(ReadData1 + immediateSE); //LW and SW	
 	end
 
 	else if(opcode == 6'b001010)begin
