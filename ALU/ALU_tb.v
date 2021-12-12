b6f9c02fb265a40aa429ed2728abb5bb17d19d2e
itmr1 <92363026+itmr1@users.noreply.github.com>
2021-12-12 16:10:30 +0000

    Update ALU_tb.v

--- ALU/ALU_tb.v
+++ ALU/ALU_tb.v
@@ -5,10 +5,46 @@ logic[31:0] ReadData1;
 logic reset;
 logic[31:0] ReadData2;
 logic[31:0] ALUResult;
+logic[3:0] byteenable;
+
+ALUbyte dut(.instruction(instruction), .ReadData1(ReadData1), .reset(reset), .ReadData2(ReadData2), .ALUResult(ALUResult), .byteenable(byteenable));
 
 initial begin
-	$dumpfile("ALU_tb_waves.vcd");
-	$dumpvars(0, ALU_tb);
+	instruction = 32'h80000006; //immediate 6
+	ReadData1 = 0;
+	reset = 0;
+	ReadData2 = 6;
+	#10;
+	assert(ALUResult == 4);
+	assert(byteenable == 4'b0100);
+	$display("LOAD BYTE: Getting byte %b from address %d", byteenable, ALUResult);
+	
+	instruction = 32'h90000012; //immediate 18
+	ReadData1 = 5;
+	reset = 0;
+	ReadData2 = 18;
+	#10;
+	assert(ALUResult == 20);
+	assert(byteenable == 4'b1000);
+	$display("LOAD BYTE UNSIGNED: Getting byte %b from address %d", byteenable, ALUResult);
+	
+	instruction = 32'h8C000011; //immediate 17
+	ReadData1 = 7;
+	reset = 0;
+	ReadData2 = 17;
+	#10;
+	assert(ALUResult == 24);
+	assert(byteenable == 4'b1111);
+	$display("LOAD WORD: Getting word from address %d", ALUResult);
+	
+	instruction = 32'hA000001D; //immediate 29
+	ReadData1 = 20;
+	reset = 0;
+	ReadData2 = 29;
+	#10;
+	assert(ALUResult == 48);
+	assert(byteenable == 4'b0010);
+	$display("STORE BYTE: Storing into byte %b in address %d", byteenable, ALUResult);
 	
 	ReadData1 = 30;
 	ReadData2 = 50;
@@ -25,9 +61,7 @@ initial begin
 	instruction=32'h8D49001E;
 	#1;
 	assert(ALUResult ==90);
+	
 end
-
-ALU dut(.instruction(instruction), .ReadData1(ReadData1), .reset(reset), .ReadData2(ReadData2), .ALUResult(ALUResult));
-
 
 endmodule
