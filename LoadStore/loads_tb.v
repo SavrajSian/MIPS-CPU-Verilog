6c14575b902a69ca9daefe473b285a324ad781fc
Soma-K <33556620+Soma-K@users.noreply.github.com>
2021-12-12 10:55:47 +0100

    Added other test cases

--- LoadStore/loads_tb.v
+++ LoadStore/loads_tb.v
@@ -9,21 +9,67 @@ loads dut(.mem_in(mem_in), .mem_out(mem_out), .instruction(instruction), .byteen
 
 initial begin
 	mem_in = 32'h00FF00FF;
-	instruction = 32'h90000000;
+	instruction = 32'h90000000;  //lbu
 	byteenable = 4'b0100;
 	#10;
 	$display("byte out is %b", mem_out);
 	assert(mem_out == 32'h000000FF);	
-	
-	mem_in = 32'h08E2F305;
-	instruction = 32'h80000000;
+
+
+	mem_in = 32'h12345678;
+	instruction = 32'h90000000;  //lbu
+	byteenable = 4'b0001;
+	#10;
+	$display("byte out is %b", mem_out);
+	assert(mem_out == 32'h00000078);	
+
+
+	mem_in = 32'h22552379;
+	instruction = 32'h90000000;  //lbu
 	byteenable = 4'b0010;
 	#10;
 	$display("byte out is %b", mem_out);
-    assert(mem_out == 32'hFFFFFFF3);
+	assert(mem_out == 32'h00000023);	
+
+
+	mem_in = 32'h12345678;
+	instruction = 32'h90000000;  //lbu
+	byteenable = 4'b1000;
+	#10;
+	$display("byte out is %b", mem_out);
+	assert(mem_out == 32'h00000012);	
+	
+	mem_in = 32'h08E2F305;
+	instruction = 32'h80000000; //lb
+	byteenable = 4'b0001;
+	#10;
+	$display("byte out is %b", mem_out);
+    	assert(mem_out == 32'h00000005);
+
+	mem_in = 32'h08E2F305;
+	instruction = 32'h80000000; //lb
+	byteenable = 4'b0010;
+	#10;
+	$display("byte out is %b", mem_out);
+    	assert(mem_out == 32'hFFFFFFF3);
+
+	mem_in = 32'h08E2F305;
+	instruction = 32'h80000000; //lb
+	byteenable = 4'b0100;
+	#10;
+	$display("byte out is %b", mem_out);
+    	assert(mem_out == 32'hEEEEEEE2);
+
+
+	mem_in = 32'h08E2F305;
+	instruction = 32'h80000000; //lb
+	byteenable = 4'b1000;
+	#10;
+	$display("byte out is %b", mem_out);
+    	assert(mem_out == 32'h00000008);
 
 	mem_in = 32'h8F4C29E7;
-	instruction = 32'h8C000000;
+	instruction = 32'h8C000000;  //lb
 	byteenable = 4'b1111;
 	#10;
 	$display("word out is %b", mem_out);
