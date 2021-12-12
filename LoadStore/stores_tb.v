ceee01130c3bebe56a1a7f6008916d748f69f382
Soma-K <33556620+Soma-K@users.noreply.github.com>
2021-12-12 11:08:08 +0100

    added test for different inputs

--- LoadStore/stores_tb.v
+++ LoadStore/stores_tb.v
@@ -7,7 +7,7 @@ logic[31:0] write_data;
 
 initial begin
 	Read_data_b = 32'hF3C8E52D;
-	instruction = 32'hAC000000;
+	instruction = 32'hAC000000; //sw
 	#10;
 	$display("Readdatain: %b", Read_data_b);
 	$display("opcode: %b", instruction[28:26]);
@@ -15,12 +15,28 @@ initial begin
 	assert(write_data == 32'hF3C8E52D);
 	
 	Read_data_b = 32'hF3C8E52D;
-	instruction = 32'hA0000000;
+	instruction = 32'hA0000000;  //sb
 	#10;
 	$display("Readdatain: %b", Read_data_b);
 	$display("opcode: %b", instruction[28:26]);
 	$display("data to be stored: %b", write_data);
 	assert(write_data == 32'h0000002D);
+
+	Read_data_b = 32'h12345678;
+	instruction = 32'hAC000000; //sw
+	#10;
+	$display("Readdatain: %b", Read_data_b);
+	$display("opcode: %b", instruction[28:26]);
+	$display("data to be stored: %b", write_data);
+	assert(write_data == 32'h12345678);
+	
+	Read_data_b = 32'h12345678;
+	instruction = 32'hA0000000;  //sb
+	#10;
+	$display("Readdatain: %b", Read_data_b);
+	$display("opcode: %b", instruction[28:26]);
+	$display("data to be stored: %b", write_data);
+	assert(write_data == 32'h00000078);
 
 end
 
