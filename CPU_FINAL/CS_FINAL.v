0e084196948c530a4f3560f90421caca1ce98a3e
Dinho-23 <94057520+Dinho-23@users.noreply.github.com>
2021-12-14 22:01:33 +0000

    Add files via upload

---
+++ CPU_FINAL/CS_FINAL.v
@@ -0,0 +1,192 @@
+module control_signals(
+    input logic [31:0] inst,
+    input logic end_of_inst_reg,
+    input logic waitrequest,
+    input logic clk,
+    input logic reset,
+    input logic end_j,
+    input logic end_of_inst_store,
+    input logic[31:0] readdata1,
+    input logic[31:0] readdata2,
+    output logic MemRead,
+    output logic MemWrite,
+    output logic ALUSrc,
+    output logic RegDest,
+    output logic RegWrite,
+    output logic MemtoReg,
+    output logic endi,
+    output logic jrtrue,
+    output logic jumptrue,
+    output logic branchtrue,
+    output logic jalr,
+    output logic link
+);
+
+logic [2:0] i_type;
+logic [5:0] op; 
+logic [3:0] jmp;
+logic [5:0] branchbits;
+////Maybe get rid of following control sigs
+
+
+assign jmp = inst[3:0];
+assign op = inst[31:26];
+assign i_type = inst[31:29];
+assign endi = end_of_inst_store || end_of_inst_reg || end_j;   
+assign branchbits = inst[20:16]; 
+
+always_comb begin 
+	if(op == 6'b000010) begin //J
+		jumptrue = 1;
+        link = 0;
+        branchtrue = 0;
+        jrtrue =0;
+        jalr = 0;
+	end
+	
+	else if(op == 6'b000011) begin //JAL
+		link = 1;
+		jumptrue = 1;
+        jalr = 0;
+        branchtrue = 0;
+        jrtrue = 0;
+	end
+	
+    else if(op==0 && jmp==8) begin //JR
+        jrtrue = 1;
+        jalr = 0;
+        branchtrue = 0;
+        jumptrue = 0;
+        link = 0;
+    end
+
+    else if(op == 0 && jmp == 9) begin //JALR
+		link = 1;
+		jrtrue = 1;
+        jalr = 1;
+        jumptrue = 0;
+        branchtrue = 0;
+	end
+	else if(op == 6'b000100) begin //BEQ
+		branchtrue = (readdata1 == readdata2) ? 1:0; 
+		jumptrue = 0;
+        jalr = 0;
+		jrtrue = 0;
+		link = 0;
+	end
+	
+	else if(op == 6'b000001 && branchbits == 5'b00001) begin //BGEZ
+		jumptrue = 0;
+        jalr = 0;
+		jrtrue = 0;
+		link = 0;
+		branchtrue = (readdata1 >= 0) ? 1:0;
+	end
+	
+	else if(op == 6'b000001 && branchbits == 5'b10001) begin //BGEZAL
+		branchtrue = (readdata1 >= 0) ? 1:0;
+        link = 1;
+        jumptrue = 0;
+        jalr = 0;
+        jrtrue = 0;
+	end
+	
+	else if(op == 6'b000111 && branchbits == 0) begin //BGTZ
+		branchtrue = (readdata1 > 0) ? 1:0;
+        jumptrue = 0;
+        jalr = 0;
+        jrtrue = 0;
+        link = 0;
+	end
+	
+	else if(op == 6'b000110 && branchbits == 0) begin //BLEZ
+		branchtrue = (readdata1 <= 0) ? 1:0; 
+        jumptrue = 0;
+        jalr = 0;
+        jrtrue = 0;
+        link = 0;
+	end
+	
+	else if(op == 6'b000001 && branchbits == 0) begin //BLTZ
+		branchtrue = (readdata1 < 0) ? 1:0;
+		jumptrue = 0;
+        jalr = 0;
+		jrtrue = 0;
+        link = 0;
+	end
+	
+	else if(op == 6'b000001 && branchbits == 5'b10000) begin //BLTZAL
+		branchtrue = (readdata1 < 0) ? 1:0;
+        jumptrue = 0;
+        jalr = 0;
+        jrtrue = 0;
+        link = 1;
+	end
+	
+	else if(op == 6'b000101) begin //BNE
+		branchtrue = (readdata1 != readdata2) ? 1:0; 
+        jumptrue = 0;
+        jalr = 0;
+        jrtrue = 0;
+        link = 0;
+	end
+
+	else begin
+		branchtrue = 0;
+		jumptrue = 0;
+        jalr = 0;
+		jrtrue = 0;
+		link = 0;
+	end
+end
+
+
+always_comb begin
+	if (reset==1) begin
+		MemRead = 0;
+		MemWrite = 0;
+		ALUSrc = 0;
+		RegDest = 1;
+		RegWrite = 0;
+		MemtoReg = 0;
+        branchtrue = 0;
+		jumptrue = 0;
+		jrtrue = 0;
+		link = 0;
+	end
+    else if (i_type==3'b100) begin //All loads follow this struct
+        MemRead = 1;
+        ALUSrc = 1;
+        RegWrite = 1;
+        MemtoReg = 1;
+        MemWrite = 0;
+        RegDest = 0;
+    end
+    else if (i_type==6'b101) begin //StoreWord
+        MemRead = 0;
+        ALUSrc = 1;
+        RegWrite = 0;
+        MemWrite = 1;
+    end
+    else if (jrtrue == 1||jumptrue == 1||branchtrue == 1) begin //JR    JALR uses same jump dest
+        MemRead = 0;
+        MemWrite = 0;
+        RegWrite = (link == 1) ? 1:0;
+        ALUSrc = 0;
+        MemtoReg = 0;
+        RegDest = 1;
+    end
+
+    else if (op==6'b000000 || i_type==001) begin //R or I-type
+        MemRead = 0;
+        ALUSrc = 0;
+        RegWrite = 1;
+        MemtoReg = 0;
+        RegDest = (op==6'b000000)?1:0;
+        MemWrite = 0;
+    end
+    
+end
+	
+
+endmodule
---
+++ CPU_FINAL/PC_FINAL.v
@@ -0,0 +1,78 @@
+module PC(
+	input active,
+	input clk,
+	input fetch,
+	input reset,
+	input logic[31:0] instruction,
+	input logic[31:0] RegData,
+    input logic link,
+	output logic[31:0] pcout,
+	output logic end_j,
+	output logic[31:0] linkaddr,
+	input logic jcontrol,
+	input logic jrcontrol,
+	input logic bcontrol
+);	
+	logic[31:0] pcin = 0;
+	logic[31:0] pcnext;
+	logic[31:0] addrtojumpto;
+	logic[3:0] pc_chunk;
+	logic[31:0] jraddr; // regfile
+	logic[31:0] jaddr; // last 26 bits of inst shift 2, concatenated w first 4 bits of next address
+	logic[31:0] braddr; // last 16 bits of inst shift 2, added to next address
+	logic signed[31:0] b_add_tmp;
+	logic[25:0] j_add_tmp;
+
+	assign pc_chunk = pcnext[31:28];
+	assign b_add_tmp = instruction[15:0]<<2;
+	assign j_add_tmp = instruction[25:0]<<2;
+
+	always_comb begin
+		if(reset == 1) begin
+			pcout = 32'hBFC00000;
+			pcin =32'hBFC00000;
+			end_j = 0;
+		end
+		else if (active == 1) begin
+			pcnext = pcin + 4;
+			linkaddr = pcnext + 4;
+			if(jcontrol == 1) begin
+				jaddr = {pc_chunk,j_add_tmp};
+				addrtojumpto = jaddr;
+				end_j = (link == 1) ? 0:1;
+			end	
+			else if(jrcontrol == 1) begin
+				jraddr = RegData;
+				addrtojumpto = jraddr;
+				end_j = (link == 1)? 0:1;
+			end
+			else if(bcontrol == 1) begin
+				braddr = pcnext+b_add_tmp;
+				addrtojumpto = braddr;
+				end_j = (link == 1)? 0:1;
+			end
+
+		end
+	end
+		
+
+	always_ff @(posedge clk) begin
+		if (active == 1) begin
+			if (fetch == 1) begin
+				if(reset == 0) begin
+					if (jrcontrol==1 ||bcontrol == 1|| jcontrol == 1) begin
+						pcout <= addrtojumpto;
+						pcin <= addrtojumpto;
+						end_j <= 1;
+					end
+					else begin
+						pcout <= pcnext;
+						pcin <= pcnext;
+						end_j <= 0;
+					end
+				end
+			end
+		end
+	end
+	
+endmodule
