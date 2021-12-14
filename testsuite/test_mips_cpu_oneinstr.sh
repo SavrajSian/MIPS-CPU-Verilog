8622f8b580e824f399b67a38c5a1adcc3e9a9aab
itmr1 <92363026+itmr1@users.noreply.github.com>
2021-12-14 18:23:19 +0000

    Add files via upload

--- testsuite/test_mips_cpu_oneinstr.sh
+++ testsuite/test_mips_cpu_oneinstr.sh
@@ -7,9 +7,9 @@ TESTCASE="$2"
 
 >&2 echo "Compiling test-bench for ${TESTCASE}"
 
-iverilog -Wall -g 2012 -I test/testcases/ -s -I test/testcases/ ${TESTCASE}_tb -o ${TESTCASE}_tb ${srcdir}/ALU_FINAL.v ${srcdir}/CS_FINAL.v \
-${srcdir}/INSTR_REG_FINAL.v ${srcdir}/LOAD_FINAL.v ${srcdir}/MEM_TO_DATA_FINAL.v ${srcdir}/PC_FINAL.v \
-${srcdir}/REGFILE_FINAL.v ${srcdir}/STORE_FINAL.v ${srcdir}/Test_RAM.v ${srcdir}/TOP_LEVEL_CPU_FINAL.v
+iverilog -Wall -g 2012 -I"test/testcases/" -s ${TESTCASE}_tb -o ${TESTCASE}_tb "test/testcases/"${TESTCASE}.v ${srcdir}/*.v #${srcdir}/ALU_FINAL.v ${srcdir}/CS_FINAL.v \
+#${srcdir}/INSTR_REG_FINAL.v ${srcdir}/LOAD_FINAL.v ${srcdir}/MEM_TO_DATA_FINAL.v ${srcdir}/PC_FINAL.v \
+#${srcdir}/REGFILE_FINAL.v ${srcdir}/STORE_FINAL.v ${srcdir}/Test_RAM.v ${srcdir}/TOP_LEVEL_CPU_FINAL.v
 
 set +e
 ./${TESTCASE}_tb
