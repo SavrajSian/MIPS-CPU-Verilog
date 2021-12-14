39d22708fca9ce5ef499326aca97edb44cb02e66
itmr1 <92363026+itmr1@users.noreply.github.com>
2021-12-14 18:56:24 +0000

    Update test_mips_cpu_bus.sh

--- testsuite/test_mips_cpu_bus.sh
+++ testsuite/test_mips_cpu_bus.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 srcdir="$1"
 instr="$2"
 
-TESTCASES="test/testcases/*.v"
+TESTCASES="test/testcases/*.v" #Make sure testcase module names are the file basename + '_tb'. E.g Addiu123.v has module name Addiu123_tb
 
 for i in ${TESTCASES} ; do
 	TESTNAME=$(basename ${i} .v)
