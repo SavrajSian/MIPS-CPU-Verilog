7c1509a5dff7e6992014b7159d8c04c342ae6373
Soma-K <33556620+Soma-K@users.noreply.github.com>
2021-12-15 17:25:50 +0100

    Add files via upload

---
+++ Testcases/ALU_Inst/ori_tb1.v
@@ -0,0 +1,106 @@
+module ori_tb1();
+
+logic clk;
+logic reset;
+logic active;
+logic[31:0] register_v0;
+
+/* Avalon memory mapped bus controller (master) */
+logic[31:0] address;
+logic write;
+logic read;
+logic waitrequest;
+logic[31:0] writedata;
+logic[3:0] byteenable;
+logic[31:0] readdata;
+
+/*RAM control logic*/
+logic[31:0] instruction;
+logic inst_input;
+logic RAM_Reset;
+logic[7:0] inst_addr;
+
+
+initial begin //intialise clock
+	clk = 0;
+	repeat (2000) begin
+		clk = !clk;
+		#5;
+	end
+    if(active != 0) begin
+        $fatal(2, "Test-bench has not completed after 1000 clock cycles. Giving up.");
+    end
+end
+
+
+
+initial begin
+    RAM_Reset = 1;
+    #10;
+    RAM_Reset = 0;
+    inst_input = 1; /*turn on when you want to add instructions*/
+
+    inst_addr = 8'h04; 
+    instruction = 32'h24030FF0;  //add 0FF0 to $3
+    #1; 
+
+    inst_addr = 8'h08; 
+    instruction = 32'h346200FF;  // $3 and 00FF to $2
+    #1; 
+
+    inst_addr = 8'h0C; 
+    instruction = 32'h00000008; /*halt instruction*/
+    #1;
+
+    inst_input = 0; /*turn off when you finish*/
+end
+
+
+
+initial begin
+    reset = 1;
+    #10;
+    reset = 0;
+end
+
+
+
+always@(negedge active) begin
+    assert (register_v0 == 32'hFFF) else $fatal(2, "register value wrong");
+end
+
+
+
+top_level_CPU cpu_dut( /*Instantiate top_level_cpu*/
+    /* Standard signals */
+    .clk(clk),
+    .reset(reset),
+    .active(active),
+    .register_v0(register_v0),
+
+    /* Avalon memory mapped bus controller (master) */
+    .address(address),
+    .write(write),
+    .read(read),
+    .waitrequest(waitrequest),
+    .writedata(writedata),
+    .byteenable(byteenable),
+    .readdata(readdata)
+);
+
+RAM mem_dut( /*Instantiate RAM*/
+    .address(address),
+    .write(write),
+    .read(read),
+    .waitrequest(waitrequest),
+    .writedata(writedata),
+    .byteenable(byteenable),
+    .readdata(readdata),
+    .instruction(instruction),
+    .inst_input(inst_input),
+    .RAM_Reset(RAM_Reset),
+    .clk(clk),
+    .inst_addr(inst_addr)
+);
+
+endmodule
