8f5059f76b568ae0d80bdaef8ee763018d1e2283
xy2120 <73561178+xy2120@users.noreply.github.com>
2021-12-16 13:11:53 +0000

    Update JR v1.v

--- Testcases/Jump_Inst/JR v1.v
+++ Testcases/Jump_Inst/JR v1.v
@@ -20,11 +20,6 @@ logic inst_input;
 logic RAM_Reset;
 logic[7:0] inst_addr;
 
-initial begin  //only needed for debugging
-    $dumpfile("JR1_waveforms.vcd");
-    $dumpvars(0,JR_testcase_1);
-end
-
 initial begin //intialise clock
 	clk = 0;
 	repeat (2000) begin
