74c6229114c22a8dc0fde046fff1551b737f65fe
xy2120 <73561178+xy2120@users.noreply.github.com>
2021-12-16 13:05:33 +0000

    Update BLTZ v3.v

--- Testcases/Jump_Inst/BLTZ v3.v
+++ Testcases/Jump_Inst/BLTZ v3.v
@@ -20,11 +20,6 @@ logic inst_input;
 logic RAM_Reset;
 logic[7:0] inst_addr;
 
-initial begin  //only needed for debugging
-    $dumpfile("BLTZ3_waveforms.vcd");
-    $dumpvars(0,BLTZ_testcase_3);
-end
-
 initial begin //intialise clock
 	clk = 0;
 	repeat (2000) begin
