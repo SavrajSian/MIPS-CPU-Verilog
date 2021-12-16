d3407ec18d0824807bf0bfea8817ca38cd8f3020
itmr1 <92363026+itmr1@users.noreply.github.com>
2021-12-16 21:29:43 +0000

    Update test_mips_cpu_oneinstr.sh

--- testsuite/test_mips_cpu_oneinstr.sh
+++ testsuite/test_mips_cpu_oneinstr.sh
@@ -5,17 +5,16 @@ set -eou pipefail
 srcdir="$1"
 TESTCASE="$2"
 
->&2 echo "Compiling test-bench for ${TESTCASE}"
-
-iverilog -Wall -g 2012 -I"test/testcases/" -s ${TESTCASE}_tb -o ${TESTCASE}_tb "test/testcases/"${TESTCASE}.v ${srcdir}/*.v
+iverilog -Wall -g 2012 -I"test/testcases/" -s ${TESTCASE}_tb\
+ -o ${TESTCASE}_tb "test/testcases/"${TESTCASE}.v ${srcdir}/mips_cpu_*.v ${srcdir}/mips_cpu/*.v
 
 set +e
 ./${TESTCASE}_tb
 RESULT=$?
 set -e
 
-if [[ "${RESULT}" -ne 0 ]] ; then
-   echo "${TESTCASE}, ${TESTCASE//[^[:alpha:]]/}, fail"
+if [[ "${RESULT}" -eq 0 ]] ; then
+   echo "${TESTCASE}, ${TESTCASE//[^[:alpha:]]/}, Pass"
 else
-   echo "${TESTCASE}, ${TESTCASE//[^[:alpha:]]/}, pass"
+   echo "${TESTCASE}, ${TESTCASE//[^[:alpha:]]/}, Fail"
 fi
