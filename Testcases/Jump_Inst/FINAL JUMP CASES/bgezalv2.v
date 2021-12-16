cb68e45fe1589b09675b6cd72375fdc3985a44fe
Savraj Sian <94938371+SavrajSian@users.noreply.github.com>
2021-12-16 12:29:01 +0000

    Update bgezalv2.v

--- Testcases/Jump_Inst/FINAL JUMP CASES/bgezalv2.v
+++ Testcases/Jump_Inst/FINAL JUMP CASES/bgezalv2.v
@@ -20,10 +20,7 @@ logic inst_input;
 logic RAM_Reset;
 logic[7:0] inst_addr;
 
-initial begin  //only needed for debugging
-    $dumpfile("BGEZAL2_waveforms.vcd");
-    $dumpvars(0,bgezalv2_tb);
-end
+
 
 initial begin //intialise clock
 	clk = 0;
