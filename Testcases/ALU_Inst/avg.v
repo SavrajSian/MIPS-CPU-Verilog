239eca06a01ffe1198770743fbc5e432997aeca8
Savraj Sian <94938371+SavrajSian@users.noreply.github.com>
2021-12-17 16:56:49 +0000

    Update avg.v

--- Testcases/ALU_Inst/avg.v
+++ Testcases/ALU_Inst/avg.v
@@ -20,10 +20,6 @@ logic inst_input;
 logic RAM_Reset;
 logic[7:0] inst_addr;
 
-initial begin  //only needed for debugging
-    $dumpfile("avg_waveforms.vcd");
-    $dumpvars(0,avg_tb);
-end
 
 initial begin //intialise clock
 	clk = 0;
